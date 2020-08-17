637cf265c711c780a845e1c3a5e85c854027b1c5
Zanurzeniwzieleni <zanurzeniwzieleni@gmail.com>
2020-08-17 13:52:33 +0200

    a

--- apka/urls.py
+++ apka/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
 	url(r'^search/$', views.search, name='search'),
-    url(r'^topics/$', views.topics, name='topics'),
+        url(r'^topics/$', views.topics, name='topics'),
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 	
 ]
